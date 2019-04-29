46d859827c7acc4c3c8228bdd77fbfb7fc32dc89
srkndenis <srkndenis@gmail.com>
2019-04-29 13:44:48 +0400

    Update system_prop.mk

--- product/system_prop.mk
+++ product/system_prop.mk
@@ -1,21 +1,21 @@
 # Radio
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.radio.apm_sim_not_pwdn=1 \
-	persist.radio.add_power_save=1
-	persist.radio.rat_on=combine
-	persist.radio.no_wait_for_card=1
-	persist.radio.use_se_table_only=1
-	persist.radio.jbims=1
-	ro.ril.telephony.nstrings=7
-	ro.telephony.default_network=9
-	ro.product_ship=true
-	ro.ril.transmitpower=true
-	telephony.lteOnGsmDevice=1
-	ro.ril.telephony.mqanelements=6
+	persist.radio.add_power_save=1 \
+	persist.radio.rat_on=combine \
+	persist.radio.no_wait_for_card=1 \
+	persist.radio.use_se_table_only=1 \
+	persist.radio.jbims=1 \
+	ro.ril.telephony.nstrings=7 \
+	ro.telephony.default_network=9 \
+	ro.product_ship=true \
+	ro.ril.transmitpower=true \
+	telephony.lteOnGsmDevice=1 \
+	ro.ril.telephony.mqanelements=6 \
+	ro.multisim.set_audio_params=true \
+	ro.telephony.ril.config=simactivation \
 	ro.multisim.set_audio_params=true
-	ro.telephony.ril.config=simactivation
-	ro.multisim.set_audio_params=true
-	
+
 # Screen
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=320
